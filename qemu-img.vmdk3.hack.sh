d3d4de13ff4f2710efdfd9d0b71d6a88e8747236
Erik Smit <zoiah@zoiah.net>
2014-04-11 00:07:54 +0200

    Various
    Implement @sciurus comments from issue #1

--- qemu-img.vmdk3.hack.sh
+++ qemu-img.vmdk3.hack.sh
@@ -38,13 +38,13 @@ VMDKFOOTERVER=$[$VMDKFOOTER  + 4]
 case "`xxd -ps -s $VMDKFOOTERVER -l 1 \"$VMDK\"`" in
   03) 
     echo -e "$VMDK is VMDK3.\n Patching to VMDK2.\n Run this script again when you're done to patch it back."
-    echo -en '\x02' | dd conv=notrunc status=none oflag=seek_bytes seek=$[VMDKFOOTERVER]  of="$VMDK"
+    echo -en '\x02' | dd conv=notrunc oflag=seek_bytes seek=$[VMDKFOOTERVER] of="$VMDK" 2> /dev/null || echo 'Patching failed'
     ;;
   02) 
     echo "File is VMDK2. Patching to VMDK3."
-    echo -en '\x03' | dd conv=notrunc status=none oflag=seek_bytes seek=$[VMDKFOOTERVER]  of="$VMDK"
+    echo -en '\x03' | dd conv=notrunc oflag=seek_bytes seek=$[VMDKFOOTERVER] of="$VMDK" 2> /dev/null || echo 'Patching failed'
     ;;
-  default)
-    echo "$VMDK is not VMDK3 or patched-VMDK3.\n"
+  *) # default
+    echo "$VMDK is not VMDK3 or patched-VMDK3."
   ;;
 esac
